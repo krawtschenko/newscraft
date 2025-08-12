64a0ff6a78eeb02a8ba1809f3d8c2350723465f5
krawtschenko <eugenykravchenko@gmail.com>
2025-08-12 11:51:23 +0200

    refactor: loginForm

--- app/ui/auth/loginForm.tsx
+++ app/ui/auth/loginForm.tsx
@@ -1,11 +1,15 @@
 import { BlockContent } from "@/app/ui/blockContent";
+import { Input } from "@/app/ui/input";
 
 export const LoginForm = () => {
   return (
     <BlockContent className="flex flex-col gap-6">
       <h3 className="text-[28px]">Log In</h3>
-      <input type="text" />
-      <input type="text" />
+      <Input label={"Email"} type={"email"} />
+      <Input label={"Password"} type={"password"} />
+      <button className="self-end py-1.5 w-[69px] bg-sky-600 rounded-sm cursor-pointer hover:bg-sky-800 active:bg-sky-950 text-white transition">
+        Log In
+      </button>
     </BlockContent>
   );
 };
---
+++ app/ui/input.tsx
@@ -0,0 +1,28 @@
+import { ComponentPropsWithoutRef, useId } from "react";
+
+type Props = {
+  label?: string;
+  error?: string;
+} & ComponentPropsWithoutRef<"input">;
+
+export const Input = ({ label, error, ...rest }: Props) => {
+  const id = useId();
+
+  return (
+    <div className="w-[304px]">
+      {label && (
+        <label className="block font-light" htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <input
+        className="w-full py-1.5 px-3 border border-[#DFDFDF] rounded-sm focus:outline-none hover:border-sky-300 focus:border-sky-400 transition"
+        id={id}
+        {...rest}
+      />
+      {error && (
+        <span className="block font-light text-sm text-red-600">{error}</span>
+      )}
+    </div>
+  );
+};
